c3f39c0e2d401cd99b5f45f4f0176c22beebc2f7
bakuyy <90575227+bakuyy@users.noreply.github.com>
2022-05-22 09:09:30 -0400

    Front-end files
    HTML, CSS, JS Google Extension

---
+++ background.js
@@ -0,0 +1,6 @@
+let color = 
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
